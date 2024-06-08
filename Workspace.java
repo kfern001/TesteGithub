c0a36bb05a18c4414374f2543316e62422f22725
Kaio Fernandes <kayyo.fer@gmail.com>
2024-06-08 16:16:18 -0300

    Acrescentado input de valores

--- Workspace.java
+++ Workspace.java
@@ -1,19 +1,27 @@
 import classes.Training;
+import java.util.Scanner;
 
 public class Workspace {
     public static void main(String[] args) {
-        int[] list = new int[10];
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many itens will there be in your list? ");
+        int n = sc.nextInt();
+        int[] list = new int[n];
 
         for (int x = 0; x < list.length; x++) {
-            list[x] = (x + 1);
+            System.out.print("Type number #" + (x + 1) + ": ");
+            int number = sc.nextInt();
+            list[x] = (number);
         }
 
         Training classList = new Training();
         classList.setNumberList(list);
 
-        for (int x : classList.getNumberList()) {
-            System.out.println(x);
+        System.out.println("Number from the list: ");
+        for (int x = 0; x < list.length; x++) {
+            System.out.println("Number #" + (x + 1) + ": " + classList.getNumberList()[x]);
         }
 
+        sc.close();
     }
 }
